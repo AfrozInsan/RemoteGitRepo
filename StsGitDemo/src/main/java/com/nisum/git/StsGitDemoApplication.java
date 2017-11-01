fa525ea7a5df4687196f6c2fddc4f56db309ffad
rajaTL <TL@gmail.com>
2017-11-01 09:28:43 +0500

    TL kept the project StsGitDemo.

---
+++ StsGitDemo/src/main/java/com/nisum/git/StsGitDemoApplication.java
@@ -0,0 +1,12 @@
+package com.nisum.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsGitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsGitDemoApplication.class, args);
+	}
+}
---
+++ StsGitDemo/src/main/java/com/nisum/git/domain/Product.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.nisum.git.domain;
+
+import java.io.Serializable;
+
+/**
+ * @author nisum
+ *
+ */
+public class Product implements Serializable {
+	
+	private int productId;
+	private String productName;
+	private double productPrice;
+	
+	
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+	
+	
+}
