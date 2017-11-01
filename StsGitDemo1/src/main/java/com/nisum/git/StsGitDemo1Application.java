0209acfc8a0c5bc351803aee53b2d36fbb2d5a86
rajaTL <TL@gmail.com>
2017-11-01 10:51:23 +0500

    tl pushed proj StsGitDemo1

---
+++ StsGitDemo1/src/main/java/com/nisum/git/StsGitDemo1Application.java
@@ -0,0 +1,12 @@
+package com.nisum.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsGitDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsGitDemo1Application.class, args);
+	}
+}
